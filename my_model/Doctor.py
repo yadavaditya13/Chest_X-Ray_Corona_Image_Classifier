161454c44c6d6147a18bdc1c510194973f8d20a0
Aditya Yadav <aditya78624@gmail.com>
2020-04-25 00:35:39 +0530

    Model Completed

---
+++ my_model/Doctor.py
@@ -0,0 +1,66 @@
+# importing required packages
+from keras.models import Sequential
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation
+from keras.layers.core import Dense
+from keras.layers.core import Dropout
+from keras.layers.core import Flatten
+from keras import backend as bk
+
+
+class Doctor:
+    @staticmethod
+    def classify(width, height, depth, classes):
+
+        # initialize the model along with input shape to be channels last and channels dimension itself
+        model = Sequential()
+        inputShape = (height, width, depth)
+        chanDim = -1
+
+        # if we are using "channels first", update the input shape
+        # and channels dimension
+
+        if bk.image_data_format() == "channels_first":
+            inputShape = (depth, height, width)
+            chanDim = 1
+
+        # Adding layers to our CNN
+        # first CONV => RELU => CONV => RELU => POOL layer set
+
+        model.add(Conv2D(16, (3, 3), padding="same", input_shape=inputShape))
+        model.add(Activation(activation="relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(16, (3, 3), padding="same"))
+        model.add(Activation(activation="relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(rate=0.25))
+
+        # second CONV => RELU => CONV => RELU => POOL layer set
+
+        model.add(Conv2D(16, (3, 3), padding="same"))
+        model.add(Activation(activation="relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(Conv2D(16, (3, 3), padding="same"))
+        model.add(Activation(activation="relu"))
+        model.add(BatchNormalization(axis=chanDim))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(rate=0.25))
+
+        # first (and only) set of FC => RELU layers
+
+        model.add(Flatten())
+        model.add(Dense(units=64))
+        model.add(Activation(activation="relu"))
+        model.add(BatchNormalization())
+        model.add(Dropout(rate=0.5))
+
+        # softmax classifier
+
+        model.add(Dense(classes))
+        model.add(Activation(activation="relu"))
+
+        # returning the model
+        return model
